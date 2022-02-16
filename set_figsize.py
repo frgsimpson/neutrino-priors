75d9c5dd4715b6dff74218f5cc2c8ee377f2d5ca
Katie Short <katie@Katies-MacBook-Pro.local>
2022-02-16 11:16:18 +0100

    Add figure size

---
+++ set_figsize.py
@@ -0,0 +1,35 @@
+def set_size(width, fraction=1, subplots=(1, 1)):
+    """Set figure dimensions to avoid scaling in LaTeX.
+
+    Parameters
+    ----------
+    width: float
+            Document textwidth or columnwidth in pts (440pt for JCAP)
+    fraction: float, optional
+            Fraction of the width which you wish the figure to occupy
+
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+    # Width of figure (in pts)
+    fig_width_pt = width * fraction
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    # https://disq.us/p/2940ij3
+    #golden_ratio = (5**.5 - 1) / 2
+    # matplotlib default ratio
+    golden_ratio = 4/6
+    #golden_ratio = 3/6
+    
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
+
+    return (fig_width_in, fig_height_in)
